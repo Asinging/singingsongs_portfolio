fd98e31edb1d8b98a94f3876f2e06fcee067521d
Singingsongs <ejesunday2@gmail.com>
2024-03-10 06:05:03 +0100

    email sender not fix

--- controllers/SendEmailController/index.js
+++ controllers/SendEmailController/index.js
@@ -1,21 +1,21 @@
 const emailSenderModule = require('../../emailer.js');
 
-exports.sendEmail = async (req, res, next) => {
-   if (!req.body) {
-      res.send('bad');
-      throw Error('Missing');
-   }
+exports.sendEmail = async(req, res, next) => {
+    if (!req.body) {
+        res.send('bad');
+        throw Error('Missing');
+    }
 
-   let requestBody = req.body;
+    let requestBody = req.body;
 
-   try {
-      let email = await emailSenderModule(requestBody);
-      res.json({
-         data: email,
-         message: 'Email successfull sent',
-         status: 200
-      });
-   } catch (err) {
-      res.status(500).send('Something broke!');
-   }
+    try {
+        let email = await emailSenderModule(requestBody);
+        res.json({
+            data: email,
+            message: 'Email successfull sent',
+            status: 200
+        });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
